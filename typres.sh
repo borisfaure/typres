457c32b72e35b93563c3fcdf6bd21613025e2a19
Boris Faure <billiob@gmail.com>
2013-10-17 22:34:33 +0200

    slides with bullets \o/

--- typres.sh
+++ typres.sh
@@ -3,24 +3,93 @@
 set -e
 #set -x
 
-WL=0
-
 r() {
     echo -ne '\033}qs\0' && read -s s
     COLUMNS=${s/;*}
     LINES=${s#*;}
     LINES=${LINES/;*}
-    WL=0
+    WL=1
 }
 
+PWD=`pwd`
+
 t() {
-    #TODO
-    echo "$@"
+    local str=$@
+    local len=$COLUMNS
+    local line=""
+    local lines=()
+    for w in $str; do
+        local l=${#w}
+        if [[ l -lt $len ]] ; then
+            len=$(( $len - $l - 1 ))
+            line="$line $w"
+        else
+            len=$COLUMNS
+            lines+=("$line")
+            line=""
+        fi
+    done;
+    if [[ -n $line ]]; then
+        lines+=("$line")
+    fi
+    for l in "${lines[@]}"; do
+        WL=$(( $WL + 1 ))
+        echo "$l"
+    done
 }
 
 function b {
-    #TODO
-    echo "$@"
+    local bullets=( "$@" )
+
+    local L=0
+    for str in "${bullets[@]}"; do
+       local len=$(( $COLUMNS - 2 ))
+       local line=""
+       for w in $str; do
+           local l=${#w}
+           if [[ l -lt $len ]] ; then
+               len=$(( $len - $l - 1 ))
+               line="$line $w"
+           else
+               len=$(( $COLUMNS - 2 ))
+               L=$(( $L + 1))
+               line=""
+           fi
+       done
+       if [[ -n $line ]]; then
+           L=$(( $L + 1))
+       fi
+    done
+
+    local l=$(( ($LINES - $WL - $L) / 2 ))
+    for I in $(seq 1 $l); do
+        echo ""
+        WL=$(( $WL + 1 ))
+    done
+
+    local L=0
+    for str in "${bullets[@]}"; do
+       echo -ne '\033}it#2;1;'$PWD'/item_00.png\0'
+       echo -ne '\033}ib\0##\033}ie\0'
+       local len=$(( $COLUMNS - 2 ))
+       local line=""
+       for w in $str; do
+           local l=${#w}
+           if [[ l -lt $len ]] ; then
+               len=$(( $len - $l - 1))
+               line="$line $w"
+           else
+               len=$(( $COLUMNS - 2 ))
+               echo "$line"
+               WL=$(( $WL + 1 ))
+               line="   $w"
+           fi
+       done
+       if [[ -n $line ]]; then
+           echo "$line"
+           WL=$(( $WL + 1 ))
+       fi
+    done
 }
 
 vc() {
@@ -41,12 +110,12 @@ c() {
     for w in $str; do
         local l=${#w}
         if [[ l -lt $len ]] ; then
-            len=$(( $len - $l ))
+            len=$(( $len - $l - 1 ))
             line="$line $w"
         else
             len=$COLUMNS
             lines+=("$line")
-            line=""
+            line="$w"
         fi
     done;
     if [[ -n $line ]]; then
@@ -78,26 +147,39 @@ f() {
 function s01 {
     c "Terminology - Oct 2013 - Where are we?"
 
-    f $@
+    #local h=$(( $LINES - $WL - 1 ))
+    #tycat -c -g ${COLUMNS}x9 terminology.png
+    #WL=$(( $WL + 9 ))
+}
+function s_end {
+    c "Questions ?"
+
+    #local h=$(( $LINES - $WL - 1 ))
+    #tycat -c -g ${COLUMNS}x9 terminology.png
+    #WL=$(( $WL + 9 ))
 }
 function s02 {
-    t "whoami"
+    t "$ whoami"
 
-    b "Boris Faure aka billiob"
-    b "developer on Enlightement and the EFL for 4 years"
-
-    f $@
+    local bullets=("Boris Faure aka billiob" \
+    "developer on Enlightement and the EFL for 4 years" \
+    "working on terminology for more than a year")
+    b "${bullets[@]}"
 }
 
 
 
 
-slides=(s01 s02)
+slides=(s01 s02 s_end)
 
 clear
 
+
+
+
 S=0
 while true; do
+    clear
     r
     if [[ $S -ge ${#slides[@]} ]]; then
         S=0
@@ -106,14 +188,16 @@ while true; do
         S=$(( ${#slides[@]} - 1))
     fi
 
-    eval ${slides[$S]} "$S/${#slides[@]}"
+    eval ${slides[$S]}
+
+    f "$(($S+1))/${#slides[@]}"
     read answer
     case $answer in
         q*) exit;;
         n*) S=$(( $S + 1));;
         p*) S=$(( $S - 1));;
         r*) ;;
-        *) S=$answer;;
+        *) S=$(( $answer - 1));;
     esac
 
 done
